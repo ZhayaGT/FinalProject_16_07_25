76719d64143acee7cb8100ec38948bdff0d7e28e
Kakiow <xcexc111@gmail.com>
2025-10-02 15:07:59 +0700

    Penambahan variabel baris 3

--- Test_16_07_25.java
+++ Test_16_07_25.java
@@ -1,6 +1,6 @@
 public class Test_16_07_25 {
     public static void main(String[] args) {
-        System.out.println("Aku Jonathan Absen 16");
+        System.out.println("Aku Jonathan Absen 16"); int a=1;
         System.out.println("Aku Oktavian Absen 25");
         System.out.println("Aku Christian Absen 06");
     }
