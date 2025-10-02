11690321f1ff9a6439db8c85f056eb57e2984386
ChristianNugraha06 <christnugraha5@gmail.com>
2025-10-02 15:16:13 +0700

    perubahan text pada baris 5

--- Test_16_07_25.java
+++ Test_16_07_25.java
@@ -2,6 +2,6 @@ public class Test_16_07_25 {
     public static void main(String[] args) {
         System.out.println("Aku Jonathan Absen 16, dan sangat tampan");
         System.out.println("Aku Oktavian Absen 25, minus dikit");
-        System.out.println("Aku Christian Absen 06");
+        System.out.println("Aku Christian Absen 06, dan baik hati");
     }
 }
