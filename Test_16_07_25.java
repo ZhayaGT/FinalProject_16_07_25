2d9fe98917fc360dca76c314cdca5dc789306f8b
Kakiow <xcexc111@gmail.com>
2025-10-02 15:03:03 +0700

    perubahan nama oktavian

--- Test_16_07_25.java
+++ Test_16_07_25.java
@@ -1,5 +1,6 @@
 public class Test_16_07_25 {
     public static void main(String[] args) {
         System.out.println("Aku Jonathan Absen 16");
+        System.out.println("Aku Oktavian Absen 25");
     }
 }
